2d3a7990224513f2e284e4bd4b3e5da50ed381c9
ummarikar <ummarikar@gmail.com>
2021-08-12 00:28:15 +0100

    add configs. write model in myproject.cpp

--- template-test/firmware/myproject.cpp
+++ template-test/firmware/myproject.cpp
@@ -23,19 +23,19 @@
 
 void myproject(
     input_t input_1[N_INPUT_1_1*N_INPUT_2_1],
-    result_t layer5_out[N_LAYER_3],
+    result_t layer6_out[N_INPUT_1_1*N_INPUT_2_1],
     unsigned short &const_size_in_1,
     unsigned short &const_size_out_1
 ) {
 
     //hls-fpga-machine-learning insert IO
     //#pragma HLS ARRAY_RESHAPE variable=input_1 complete dim=0
-    #pragma HLS ARRAY_PARTITION variable=layer5_out complete dim=0
-    #pragma HLS INTERFACE ap_vld port=input_1,layer5_out 
+    #pragma HLS ARRAY_PARTITION variable=layer6_out complete dim=0
+    #pragma HLS INTERFACE ap_vld port=input_1,layer6_out 
     #pragma HLS DATAFLOW 
 
     const_size_in_1 = N_INPUT_1_1*N_INPUT_2_1;
-    const_size_out_1 = N_LAYER_3;
+    const_size_out_1 = N_INPUT_1_1*N_INPUT_2_1;
 
 #ifndef __SYNTHESIS__
     static bool loaded_weights = false;
@@ -58,26 +58,22 @@ void myproject(
     const int lstm2_factor = N_INPUT_1_1;
     #pragma HLS ARRAY_PARTITION variable=input_1 cyclic factor=lstm2_factor
 
-    layer2_t layer2_out[N_SEQUENCE_OUT_2*N_LAYER_2];
+    layer2_t layer2_out[N_INPUT_1_1*N_LAYER_2];
     #pragma HLS ARRAY_PARTITION variable=layer2_out complete dim=0
-    nnet::lstm_05_025<input_t, input_t, config1_lstm2, config2_lstm2, config_x_lstm2, config_h_lstm2>(input_1, w2, wr2, b2, layer2_out);
+    nnet::lstm_06_025<input_t, input_t, config1_lstm2, config2_lstm2, config_x_lstm2, config_h_lstm2>(input_1, lstm1, lstm2, lstm3, layer2_out);
 
-    
-
-    layer3_t layer3_out[N_SEQUENCE_OUT_3*N_LAYER_3];
+    layer3_t layer3_out[N_INPUT_1_1*N_LAYER_2];
     #pragma HLS ARRAY_PARTITION variable=layer3_out complete dim=0
-    nnet::repeat_vector<layer2_t, layer3_t, config3>(layer2_out, layer3_out, w3, b3);
+    nnet::repeat_vector<layer2_t, layer3_t, config3>(layer2_out, layer3_out);
 
-    layer4_t layer4_out[N_SEQUENCE_OUT_4*N_LAYER_4];
+    layer4_t layer4_out[N_INPUT_1_1*N_LAYER_2];
     #pragma HLS ARRAY_PARTITION variable=layer4_out complete dim=0
-    nnet::lstm<layer2_t, layer3_t, config3>(layer2_out, layer3_out, w3, b3);
+    nnet::lstm_06_025<layer3_t, layer4_t, config1_lstm4, config2_lstm4, config_x_lstm4, config_h_lstm4>(layer3_out, lstm_11, lstm_12, lstm_13, layer4_out);
 
-    layer5_t layer5_out[N_SEQUENCE_OUT_5*N_LAYER_5];
+    layer5_t layer5_out[N_INPUT_1_1*N_LAYER_2];
     #pragma HLS ARRAY_PARTITION variable=layer5_out complete dim=0
-    nnet::attention<layer2_t, layer3_t, config3>(layer2_out, layer3_out, w3, b3);
+    nnet::attention<layer4_t, layer5_t, config5>(layer4_out, layer5_out);
 
-    layer6_t layer6_out[N_SEQUENCE_OUT_6*N_LAYER_6];
-    #pragma HLS ARRAY_PARTITION variable=layer6_out complete dim=0
-    nnet::td_dense<layer2_t, layer3_t, config3>(layer2_out, layer3_out, w3, b3);
+    nnet::td_dense<layer5_t, result_t, config6>(layer5_out, layer6_out, time_distributed1, time_distributed2);
 
 }
--- template-test/firmware/parameters.h
+++ template-test/firmware/parameters.h
@@ -9,12 +9,18 @@
 #include "nnet_utils/nnet_lstm.h"
 #include "nnet_utils/nnet_activation.h"
 #include "nnet_utils/nnet_dense.h"
+#include "nnet_utils/nnet_repeat_vector.h"
+#include "nnet_utils/nnet_attention.h"
+#include "nnet_utils/nnet_time_distributed_dense.h"
 
-#include "weights/w2.h"
-#include "weights/b2.h"
-#include "weights/wr2.h"
-#include "weights/w3.h"
-#include "weights/b3.h"
+#include "weights/lstm1.h"
+#include "weights/lstm2.h"
+#include "weights/lstm3.h"
+#include "weights/lstm_11.h"
+#include "weights/lstm_12.h"
+#include "weights/lstm_13.h"
+#include "weights/time_distributed1.h"
+#include "weights/time_distributed2.h"
 
 struct config1_lstm2 : nnet::lstm_config {
     static const unsigned length_x = N_INPUT_2_1;
@@ -62,7 +68,33 @@ struct config_h_lstm2 : nnet::dense_config {
     static const unsigned n_out = N_LAYER_2 * 4;
 };
 
-struct config3 : nnet::dense_config {
+struct config3 : nnet::repeat_vector_config {
+    // Layer Sizes
+    static const unsigned h = N_LAYER_2;
+    static const unsigned t = N_SEQUENCE_OUT_2;
+
+};
+
+struct config1_lstm4 : nnet::lstm_config {
+    static const unsigned length_x = N_LAYER_2;
+    static const unsigned length_h = N_LAYER_2;
+    static const unsigned timestep = N_INPUT_1_1;
+   
+    static const unsigned reuse_factor_tail = 1;
+
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef model_default_t accum_t;
+    typedef model_default_t mult_t;
+};
+
+struct config2_lstm4 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_2;
+    typedef ap_fixed<18,8> table_t;
+    typedef model_default_t constant_t;
+};
+
+struct config_x_lstm4 : nnet::dense_config {
     // Internal data type definitions
     typedef model_default_t weight_t;
     typedef model_default_t bias_t;
@@ -71,11 +103,45 @@ struct config3 : nnet::dense_config {
 
     // Layer Sizes
     static const unsigned reuse_factor = 1;
-    static const unsigned n_in = N_SEQUENCE_OUT_2*N_LAYER_2;
-    static const unsigned n_out = N_LAYER_3;
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned n_out = N_LAYER_2 * 4;
 
 };
 
+struct config_h_lstm4 : nnet::dense_config {
+    // Internal data type definitions
+    typedef model_default_t weight_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t accum_t;
+    typedef model_default_t mult_t;
 
+    // Layer Sizes
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned n_out = N_LAYER_2 * 4;
+};
+
+struct config5 : nnet:attention_config {
+    typedef model_default_t mult_t;
+    typedef model_default_t accum_t;
+
+	static const unsigned query_h = N_INPUT_1_1;
+	static const unsigned query_x = N_LAYER_2;
+	static const unsigned value_h = N_INPUT_1_1;
+	static const unsigned value_x = N_LAYER_2;
+};
+
+struct config6 : nnet:time_distributed_dense_config {
+	typedef model_default_t weight_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t accum_t;
+    typedef model_default_t mult_t;
+
+    // Layer Sizes
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned n_out = N_INPUT_2_1;
+	static const unsigned t = N_INPUT_1_1;
+};
 
 #endif
